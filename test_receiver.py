c1d3e692514919e57c933bc7dda393fac735b89e
Baran Cengiz <baran.cengiz@metu.edu.tr>
2020-10-10 18:30:58 +0300

    Working 56 bits key exchange between server and 2 clients. Sending and receiving are done via functions

--- test_receiver.py
+++ test_receiver.py
@@ -7,6 +7,59 @@ key_size = 56
 sifted_key = []
 
 
+def create_key_rcv(Receiver):
+    while len(sifted_key) < key_size:
+        mess = Receiver.recvClassical()
+        mess = list(mess)
+        src = mess[0]
+        is_alert = None
+        if len(mess) == 1:
+            is_alert = True
+        if is_alert:
+            pair_arr = []
+            # Make an EPR pair with Alice
+            for i in range(N):
+                pair_arr.append(Receiver.recvEPR())
+            print("EPR pairs received")
+            # Get lists of measurements
+            data = Receiver.recvClassical()
+            message = list(data)
+            # print("Message ", message)
+            # Apply corrections
+            for i in range(N):
+                a = message[i]
+                b = message[N + i]
+                if b == 1:
+                    pair_arr[i].X()
+                if a == 1:
+                    pair_arr[i].Z()
+
+            # Choose random basis for each qubit at Bob's side and then
+            # measure all qubits according to those polarizations
+            B_key = [[randrange(2), None] for _ in range(N)]
+            for i in range(N):
+                if B_key[i][0]:
+                    pair_arr[i].H()
+
+            measurements = []
+            # Measure qubit
+            for i in range(N):
+                measurements.append(pair_arr[i].measure())
+            # to_print = "App {}: Measurement outcome is: {}".format(Client.name, measurements)
+            # print_fancy(to_print)
+
+            Receiver.sendClassical("Node" + str(src), [a for a, _ in B_key])
+            msg = Receiver.recvClassical()
+            msg = list(msg)
+            # print("Sifted key mask: ", msg)
+            for i in range(len(msg)):
+                if msg[i] == 1:
+                    sifted_key.append(measurements[i])
+            print("Sifted key: ", sifted_key)
+    sifted_key_f = sifted_key[:key_size]
+    return sifted_key_f
+
+
 def print_fancy(text):
     print("|" + "-" * (len(text) + 2) + "|")
     print("| " + text + " |")
@@ -15,56 +68,9 @@ def print_fancy(text):
 
 def main():
     # Initialize the connection
-    with CQCConnection("Node2") as Client:
-        while len(sifted_key) < key_size:
-            mess = Client.recvClassical()
-            mess = list(mess)
-            src = mess[0]
-            is_alert = None
-            if len(mess) == 1:
-                is_alert = True
-            if is_alert:
-                pair_arr = []
-                # Make an EPR pair with Alice
-                for i in range(N):
-                    pair_arr.append(Client.recvEPR())
-                print("EPR pairs received")
-                # Get lists of measurements
-                data = Client.recvClassical()
-                message = list(data)
-                print("Message ", message)
-                # Apply corrections
-                for i in range(N):
-                    a = message[i]
-                    b = message[N + i]
-                    if b == 1:
-                        pair_arr[i].X()
-                    if a == 1:
-                        pair_arr[i].Z()
-
-                # Choose random basis for each qubit at Bob's side and then measure all qubits according to those polarizations
-                B_key = [[randrange(2), None] for _ in range(N)]
-                for i in range(N):
-                    if B_key[i][0]:
-                        pair_arr[i].H()
-
-                measurements = []
-                # Measure qubit
-                for i in range(N):
-                    measurements.append(pair_arr[i].measure())
-                # to_print = "App {}: Measurement outcome is: {}".format(Client.name, measurements)
-                # print_fancy(to_print)
-
-                Client.sendClassical("Node" + str(src), [a for a, _ in B_key])
-                msg = Client.recvClassical()
-                msg = list(msg)
-                # print("Sifted key mask: ", msg)
-                for i in range(len(msg)):
-                    if msg[i] == 1:
-                        sifted_key.append(measurements[i])
-                print("Sifted key: ", sifted_key)
-            sifted_key_f = sifted_key[:key_size]
-            print_fancy(str(sifted_key_f))
+    with CQCConnection("Node2") as Node2:
+        key = create_key_rcv(Node2)
+        print_fancy(str(key))
 
 
 main()
--- test_sender.py
+++ test_sender.py
@@ -9,6 +9,68 @@ dest = 2
 sifted_key = []
 
 
+def create_key_snd(Snd):
+    while len(sifted_key) < key_size:
+        q_arr = []
+        request_pairs(Snd, N, dest)
+        pair_arr = []
+        A_key = generate_base_key_pair()
+        print("KEY: ", [a for _, a in A_key])
+        # Receive qubits from the server
+        for i in range(N):
+            pair_arr.append(Snd.recvQubit())
+
+        # Teleportation
+        for i in range(N):
+            # Create a qubit to teleport
+            q = qubit(Snd)
+
+            if A_key[i][1] == 1:
+                q.X()
+            if A_key[i][0] == 1:
+                q.H()
+            # Teleportation operations
+            q.cnot(pair_arr[i])
+            q.H()
+            q_arr.append(q)
+
+        measurements_a = []
+        measurements_b = []
+
+        # Measure qubits
+        for i in range(N):
+            measurements_a.append(q_arr[i].measure())
+            measurements_b.append(pair_arr[i].measure())
+
+        # # Display measurement
+        # to_print = "App {}: Measurement outcomes are: a={}, b={}".format(Snd.name, measurements_a, measurements_b)
+        # print_fancy(to_print)
+
+        # Send measurement results for teleportation
+        msg = []
+        msg.extend(measurements_a)
+        msg.extend(measurements_b)
+        Snd.sendClassical("Node" + str(dest), msg)
+        msg = Snd.recvClassical()
+        msg = list(msg)
+        sifted_key_idx = []
+        for i in range(len(msg)):
+            if A_key[i][0] == msg[i]:
+                sifted_key_idx.append(1)
+            else:
+                sifted_key_idx.append(0)
+        # print("Sifted key mask: ", sifted_key_idx)
+        Snd.sendClassical("Node" + str(dest), sifted_key_idx)
+
+        for i in range(len(sifted_key_idx)):
+            if sifted_key_idx[i] == 1:
+                sifted_key.append(A_key[i][1])
+        print("Sifted key: ", sifted_key)
+        print("Sifted key length: ", len(sifted_key))
+    sifted_key_f = sifted_key[:key_size]
+    return sifted_key_f
+
+
 def request_pairs(Src, size, dest):
     message = [0, src, dest, size]
     Src.sendClassical("Server", message)
@@ -33,66 +95,9 @@ def print_fancy(text):
 
 def main():
     # Initialize the connection
-    with CQCConnection("Node1") as Snd:
-        while len(sifted_key) < key_size:
-            q_arr = []
-            request_pairs(Snd, N, 2)
-            pair_arr = []
-            A_key = generate_base_key_pair()
-            print("KEY: ", [a for _, a in A_key])
-            # Receive qubits from the server
-            for i in range(N):
-                pair_arr.append(Snd.recvQubit())
-
-            # Teleportation
-            for i in range(N):
-                # Create a qubit to teleport
-                q = qubit(Snd)
-
-                if A_key[i][1] == 1:
-                    q.X()
-                if A_key[i][0] == 1:
-                    q.H()
-                # Teleportation operations
-                q.cnot(pair_arr[i])
-                q.H()
-                q_arr.append(q)
-
-            measurements_a = []
-            measurements_b = []
-
-            # Measure qubits
-            for i in range(N):
-                measurements_a.append(q_arr[i].measure())
-                measurements_b.append(pair_arr[i].measure())
-
-            # # Display measurement
-            # to_print = "App {}: Measurement outcomes are: a={}, b={}".format(Snd.name, measurements_a, measurements_b)
-            # print_fancy(to_print)
-
-            # Send measurement results for teleportation
-            msg = []
-            msg.extend(measurements_a)
-            msg.extend(measurements_b)
-            Snd.sendClassical("Node" + str(dest), msg)
-            msg = Snd.recvClassical()
-            msg = list(msg)
-            sifted_key_idx = []
-            for i in range(len(msg)):
-                if A_key[i][0] == msg[i]:
-                    sifted_key_idx.append(1)
-                else:
-                    sifted_key_idx.append(0)
-            # print("Sifted key mask: ", sifted_key_idx)
-            Snd.sendClassical("Node" + str(dest), sifted_key_idx)
-
-            for i in range(len(sifted_key_idx)):
-                if sifted_key_idx[i] == 1:
-                    sifted_key.append(A_key[i][1])
-            print("Sifted key: ", sifted_key)
-            print("Sifted key length: ", len(sifted_key))
-        sifted_key_f = sifted_key[:key_size]
-        print_fancy(str(sifted_key_f))
+    with CQCConnection("Node1") as Node1:
+        key = create_key_snd(Node1)
+        print_fancy(str(key))
 
 
 main()
