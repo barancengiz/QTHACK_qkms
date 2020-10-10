299747585db47c584bb3c87c21b25c6c56272bd3
Baran Cengiz <baran.cengiz@metu.edu.tr>
2020-10-10 18:09:53 +0300

    Working 56 bits key exchange between server and 2 clients

--- test_receiver.py
+++ test_receiver.py
@@ -1,46 +1,70 @@
+from random import randrange
+
 from cqc.pythonLib import CQCConnection
 
 N = 10  # Number of paired qubits
 key_size = 56
-A_key = []
+sifted_key = []
+
+
+def print_fancy(text):
+    print("|" + "-" * (len(text) + 2) + "|")
+    print("| " + text + " |")
+    print("|" + "-" * (len(text) + 2) + "|")
 
 
 def main():
     # Initialize the connection
     with CQCConnection("Node2") as Client:
+        while len(sifted_key) < key_size:
+            mess = Client.recvClassical()
+            mess = list(mess)
+            src = mess[0]
+            is_alert = None
+            if len(mess) == 1:
+                is_alert = True
+            if is_alert:
+                pair_arr = []
+                # Make an EPR pair with Alice
+                for i in range(N):
+                    pair_arr.append(Client.recvEPR())
+                print("EPR pairs received")
+                # Get lists of measurements
+                data = Client.recvClassical()
+                message = list(data)
+                print("Message ", message)
+                # Apply corrections
+                for i in range(N):
+                    a = message[i]
+                    b = message[N + i]
+                    if b == 1:
+                        pair_arr[i].X()
+                    if a == 1:
+                        pair_arr[i].Z()
 
-        mess = Client.recvClassical()
-        mess = list(mess)
-        is_alert = None
-        if len(mess) == 1:
-            is_alert = True
-        if is_alert:
-            pair_arr = []
-            # Make an EPR pair with Alice
-            for i in range(N):
-                pair_arr.append(Client.recvEPR())
-            print("EPR pairs received")
-            # Get lists of measurements
-            data = Client.recvClassical()
-            message = list(data)
-            print("Message ", message)
-            # Apply corrections
-            for i in range(N):
-                a = message[i]
-                b = message[N + i]
-                if b == 1:
-                    pair_arr[i].X()
-                if a == 1:
-                    pair_arr[i].Z()
+                # Choose random basis for each qubit at Bob's side and then measure all qubits according to those polarizations
+                B_key = [[randrange(2), None] for _ in range(N)]
+                for i in range(N):
+                    if B_key[i][0]:
+                        pair_arr[i].H()
 
-            measurements = []
-            # Measure qubit
-            for i in range(N):
-                measurements.append(pair_arr[i].measure())
-            to_print = "App {}: Measurement outcome is: {}".format(Client.name, measurements)
-            print("|" + "-" * (len(to_print) + 2) + "|")
-            print("| " + to_print + " |")
-            print("|" + "-" * (len(to_print) + 2) + "|")
+                measurements = []
+                # Measure qubit
+                for i in range(N):
+                    measurements.append(pair_arr[i].measure())
+                # to_print = "App {}: Measurement outcome is: {}".format(Client.name, measurements)
+                # print_fancy(to_print)
+
+                Client.sendClassical("Node" + str(src), [a for a, _ in B_key])
+                msg = Client.recvClassical()
+                msg = list(msg)
+                # print("Sifted key mask: ", msg)
+                for i in range(len(msg)):
+                    if msg[i] == 1:
+                        sifted_key.append(measurements[i])
+                print("Sifted key: ", sifted_key)
+            sifted_key_f = sifted_key[:key_size]
+            print_fancy(str(sifted_key_f))
 
 
 main()
--- test_server.py
+++ test_server.py
@@ -13,35 +13,36 @@ def main():
     N = 0  # Number of qubit pairs
     source = 0
     with CQCConnection("Server") as Server:
-        # Get required pair size
-        request = Server.recvClassical()
-        request_type = request[0]
-        source = node_map(request[1])
-        destination = node_map(request[2])
-        N = request[3]
-        if N < 1:
-            print("Error: Invalid number of pairs {}".format(N))
-            exit(0)
-        # Request Approved
-        print("Request Approved")
-        Server.sendClassical(source, 1)
-        print("Response sent to source")
-        Server.sendClassical(destination, request[1])
-        print("Response sent to destination")
+        while True:
+            # Get required pair size
+            request = Server.recvClassical()
+            request_type = request[0]
+            source = node_map(request[1])
+            destination = node_map(request[2])
+            N = request[3]
+            if N < 1:
+                print("Error: Invalid number of pairs {}".format(N))
+                exit(0)
+            # Request Approved
+            # print("Request Approved")
+            Server.sendClassical(source, 1)
+            # print("Response sent to source")
+            Server.sendClassical(destination, request[1])
+            # print("Response sent to destination")
 
-        # Type0: Make pair
-        if request_type == 0:
+            # Type0: Make pair
+            if request_type == 0:
 
-            pair_arr = []
-            # Create EPR pairs, 2*size qubits
-            # Split pairs and send to source node
-            for i in range(N):
-                pair_arr.append(Server.createEPR(destination))
-                print("{} Pairs created".format((i + 1) * 2))
+                pair_arr = []
+                # Create EPR pairs, 2*size qubits
+                # Split pairs and send to source node
+                for i in range(N):
+                    pair_arr.append(Server.createEPR(destination))
+                    print("{} Pairs created".format((i + 1) * 2))
 
-            # Send remaining qubits to destination node
-            for i in range(N):
-                Server.sendQubit(pair_arr[i], source)
+                # Send remaining qubits to destination node
+                for i in range(N):
+                    Server.sendQubit(pair_arr[i], source)
 
 
 ##################################################################################################
